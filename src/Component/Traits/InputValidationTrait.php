a76c99990a81106ec3c0583cecd6aa586fbed70b
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-05-20 19:49:59 +0200

    Add components to DaisyUi, Flowbite and Preline UI libraries.

---
+++ src/Component/Traits/InputValidationTrait.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Lagdo\UiBuilder\DaisyUi\Component\Traits;
+
+use Lagdo\UiBuilder\Component\Component;
+use Lagdo\UiBuilder\Component\Html\Element;
+use Lagdo\UiBuilder\Component\Html\Html;
+use Lagdo\UiBuilder\Component\HtmlElement;
+
+trait InputValidationTrait
+{
+    /**
+     * @param string $name
+     * @param array $arguments
+     *
+     * @return HtmlElement
+     */
+    abstract protected function newElement(string $name, array $arguments = []): HtmlElement;
+
+    /**
+     * @param Element|Component $sibling
+     *
+     * @return static
+     */
+    abstract protected function addNextSibling(Element|Component $sibling): static;
+
+    /**
+     * @param HtmlElement $wrapper
+     *
+     * @return static
+     */
+    abstract protected function addWrapper(HtmlElement $wrapper): static;
+
+    /**
+     * @param bool $valid
+     * @param string $message
+     *
+     * @return static
+     */
+    public function feedback(bool $valid, string $message = ''): static
+    {
+        $this->addClass($valid ? 'validator' : 'validator invalid');
+        if ($message !== '') {
+            $element = $this->newElement('div', ['class' => 'validator-hint'])
+                ->addChild(new Html($message));
+            $this->addNextSibling($element);
+            $this->addWrapper($this->newElement('fieldset', ['class' => 'fieldset']));
+        }
+        return $this;
+    }
+
+    /**
+     * @param bool $valid
+     * @param string $message
+     *
+     * @return static
+     */
+    public function tooltip(bool $valid, string $message = ''): static
+    {
+        $this->addWrapper($this->newElement('div', [
+            'class' => $valid ? 'tooltip tooltip-success' : 'tooltip tooltip-error',
+        ]));
+        if ($message !== '') {
+            $content = $this->newElement('div', ['class' => 'tooltip-content'])
+                ->addChild(new Html($message));
+            $this->addPrevSibling($content);
+        }
+        return $this;
+    }
+}
